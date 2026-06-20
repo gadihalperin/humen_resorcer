42c050ebed29911ba583a674d1965111f1bfabfb
Gad <gadihalperin@gmail.com>
2026-06-20 11:10:04 +0300

    Refactor employee representation: replaced dictionary with Employee class for better structure and added display functionality.

--- firm_management.py
+++ firm_management.py
@@ -1,3 +1,5 @@
+"""
+
 def display_employee(emp_dict):
     print(f"Employee Name: {emp_dict['name']}\nEmployee Role: {emp_dict['role']}\nEmployee Salary: {emp_dict['base_salary']}")
 
@@ -5,3 +7,19 @@ def display_employee(emp_dict):
 
 employee_one = {"name": "Dany", "employee_id": "157345", "role" : "Portfolio Manager", "base_salary" : 2000}
 display_employee(employee_one)
+
+"""
+
+
+
+class Employee:
+    def __init__ (self, name, employee_id, role, base_salary):
+        self.name = name
+        self.employee_id = employee_id
+        self.role = role
+        self.base_salary = base_salary
+       
+
+employee_one = Employee("Dany", "157345", "Portfolio Manager", 2000)
+print(f"{employee_one.name} Salary is {employee_one.base_salary}")
+    
