089f1318ca68f7f132ead8f4f49f4920677ebc01
Gad <gadihalperin@gmail.com>
2026-06-20 12:07:35 +0300

    Add Administrative class and enhance PortfolioManager instances

--- firm_management.py
+++ firm_management.py
@@ -23,8 +23,20 @@ class PortfolioManager(Employee):
     def __init__ (self,name, employee_id, role, base_salary, aum):
         super().__init__(name, employee_id, role, base_salary)
         self.aum = aum
+
+class Administrative(Employee):
+    def __init__ (self,name, employee_id, role, base_salary, responsibilities):
+        super().__init__(name, employee_id, role, base_salary)
+        self.responsibilities = responsibilities
        
 
 pm_dany = PortfolioManager("Dany", "157345", "Portfolio Manager", 2000, 50000000)
-print(f"{pm_dany.name} Salary is {pm_dany.base_salary} and he manage {pm_dany.aum}")
-    
+pm_rami = PortfolioManager("Rami", "167333", "Portfolio Manager", 2000, 45000000)
+pm_elis = PortfolioManager("Elis", "294300", "Portfolio Manager", 2800, 65000000)
+print(f"{pm_dany.name} Salary is {pm_dany.base_salary} and he manages {pm_dany.aum}")
+print(f"{pm_rami.name} Salary is {pm_rami.base_salary} and he manages {pm_rami.aum}")
+print(f"{pm_elis.name} Salary is {pm_elis.base_salary} and he manages {pm_elis.aum}")
+
+ad_yael = Administrative("Yael", "963114", "Manage Front Desk", 1650, ["Front desk's employees", "Bank's connections"])
+ad_yaron = Administrative("Yaron", "593169", "IT", 3650, ["Data Accuracy", "Computers", "Bookkeeping"])
+print(f"{ad_yaron.name} Salary is {ad_yaron.base_salary} and his responsibilities are:\n{ad_yaron.responsibilities}")    
